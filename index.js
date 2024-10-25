9cde87f70cacad13ca3d1054fcf8a4081e45e61e
rafi669god <rafiqulislam6201@gmail.com>
2024-10-25 12:25:45 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("rafi")
+let num;
+document.getElementById("b1").onclick = function(){
+  num =document.getElementById("in1").value;
+  document.getElementById("zero").textContent =num
+
+}
